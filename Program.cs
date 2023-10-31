32897f2a8057821fb72cbd896ff4beb364dad69e
Nikola <mdamjanovic2010@gmail.com>
2023-10-31 08:25:50 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Sase Marjanovic");
+        }
+    }
+}
